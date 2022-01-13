09abbd960c765933f49dbc9fad5d3b5e3b2e25c2
đttha <dothithuha140@gmail.com>
2022-01-13 12:57:37 +0700

    ha

--- src/App.js
+++ src/App.js
@@ -4,64 +4,48 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Result from './components/Result';
-import Home from './pages/home';
 import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
 import Map from './pages/Map';
-import Document from './pages/Document';
-
+import System from './pages/System';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 export const ModalContext = createContext();
 function App() {
   const [visible, setVisible] = useState(false);
   const [content, setContent] = useState('');
   useEffect(() => {
-    console.log('visi', visible);
-  });
+    // window.addEventListener(
+    //   'scroll',
+    //   function (e) {
+    //     const nav = document.getElementById('navbar');
+    //     if (window.scrollY > 0) {
+    //       nav.classList.add('sticky');
+    //     } else {
+    //       nav.classList.remove('sticky');
+    //     }
+    //   },
+    //   { passive: true },
+    // );
+  }, []);
   return (
     <BrowserRouter>
       <ModalContext.Provider
         value={{ visible, setVisible, setContent }}
       >
         <div className='app'>
-          <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-            <div className='container-fluid'>
-              <Link className='navbar-brand' to='/'>
-                <img src="/logo.png" style={{ height: 40, width: 40 }}></img>
-              </Link>
-              <button
-                className='navbar-toggler'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#navbarScroll'
-                aria-controls='navbarScroll'
-                aria-expanded='false'
-                aria-label='Toggle navigation'
-              >
-                <span className='navbar-toggler-icon'></span>
-              </button>
-              <div
-                className='collapse navbar-collapse'
-                id='navbarScroll'
-              >
-                <ul className='navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll'>
-                  <li className='nav-item'>
-                    <Link
-                      className='nav-link active'
-                      aria-current='page'
-                      to='/'
-                    >
-                      Trang chủ
-                    </Link>
-                  </li>
-                  <li className='nav-item'>
-                    <Link className='nav-link' to='/document'>
-                      Tài liệu
-                    </Link>
-                  </li>
-                  <li className='nav-item'>
-                    <Link className='nav-link' to='/map'>
-                      Map
-                    </Link>
-                  </li>
+          {/* Thanh điều hướng */}
+          <nav className="navbar navbar-expand-lg navbar-light sticky-top py-3" style={{ backgroundColor: '#6B6361' }} id="mainNav">
+            <div className="container px-4 px-lg-5">
+              <Link className="navbar-brand" to="/">Hệ Thống Lái Xe Tự Động</Link>
+              <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+              <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ms-auto my-2 my-lg-0">
+                  <li className="nav-item"><Link className="nav-link" to="/">Giới thiệu</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/system">Hệ thống</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/map">Bản đồ</Link></li>
+                  <li className="nav-item"><Link className="nav-link" to="/contact">Liên hệ</Link></li>
                 </ul>
               </div>
             </div>
@@ -69,8 +53,9 @@ function App() {
 
           <Routes>
             <Route path='/' element={<Home />}></Route>
+            <Route path='/system' element={<System />}></Route>
             <Route path='/map' element={<Map />}></Route>
-            <Route path='/document' element={<Document />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
           </Routes>
         </div>
         <Result content={content} setContent={setContent}></Result>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,64 +1,47 @@
-import { Collapse } from 'antd';
 import React from 'react';
-import Target from '../../components/Target';
-import Connering from '../../components/Connering';
-import AvoidCollision from '../../components/AvoidCollision';
-import { Table, Space, Typography } from 'antd';
-const { Text } = Typography;
+import { Link } from 'react-router-dom';
+import image from '../../assets/image/anh1.png'
 
-const { Panel } = Collapse;
-export default function Home() {
-  const columns = [
-    {
-      title: 'Luật',
-      dataIndex: 'rule',
-      key: 'rule',
-    },
-    {
-      title: 'Mô tả',
-      dataIndex: 'des',
-      key: 'des',
-    },
-  ];
-
-  const data = [
-    {
-      rule: 'Hướng góc quay',
-      des: 'Bên trái (-), bên phải (+)',
-    },
-    {
-      rule: 'Dữ liệu',
-      des: 'Điền đầy đủ các dữ liệu cần thiết cho các trường hợp tương ứng',
-    },
-  ];
-
+const Home = () => {
   return (
     <div>
-      <center className='my-5'>
-        <h1> Hệ thống lái xe tự động</h1>
-      </center>
-      <div className='container'>
-        <Text type='danger' code>
-          Các luật của hệ thống
-        </Text>
-        <Table
-          className='mt-2'
-          columns={columns}
-          dataSource={data}
-          pagination={false}
-        />
-        <Collapse accordion className='mt-2'>
-          <Panel header='Đi tới mục tiêu' key='1'>
-            <Target></Target>
-          </Panel>
-          <Panel header='Đi tới khúc cua' key='2'>
-            <Connering />
-          </Panel>
-          <Panel header='Tránh va chạm' key='3'>
-            <AvoidCollision></AvoidCollision>
-          </Panel>
-        </Collapse>
-      </div>
+      <header className="masthead">
+        <div className="container px-4 px-lg-5 h-100">
+          <div className="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+            <div className="col-lg-8 align-self-end">
+              <h1 className="text-white font-weight-bold">Automatic driver assistance system using Fuzzy Logic</h1>
+              <hr className="divider" />
+            </div>
+            <div className="col-lg-8 align-self-baseline">
+              <p className="text-white-75 mb-5">A mini graduate project in Knowledge Based Systems by Do Thi Thu Ha
+                and Pham Nang
+                Hung!</p>
+              <Link className="btn btn-primary btn-xl" to="/system">Start</Link>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section class="page-section" id="services">
+        <div class="container px-4 px-lg-5">
+          <h2 class="text-center mt-0">Tri thức của hệ thống</h2>
+          <hr class="divider" />
+          <div class="row gx-4 gx-lg-5">
+            <img style={{ height: 800, }} src={image} />
+            <p class="mt-4">Hệ thống giải quyết theo 2 phương diện:</p>
+            <p class="mt-2 ml-2">- Bộ điều khiển đánh lái mô phỏng các hành vi sau của con người:</p>
+            <p class="ml-4">+ lái chiếc xe về phía mục tiêu (đi thẳng hoặc rẽ trái, phải).</p>
+            <p class="ml-4">+ lái xe quanh bất kỳ chướng ngại vật nào để tránh va chạm.</p>
+            <p class="ml-4">+ điều khiển xe dừng lại ở một hướng mong muốn.</p>
+            <p class="mt-2 ml-2">- Bộ điều khiển vận tốc mô phỏng các hành vi sau của con người:</p>
+            <p class="ml-4">+ khởi động xe từ điểm dừng hoàn toàn và dừng xe khi xe đến mục tiêu.</p>
+            <p class="ml-4">+ giảm tốc độ xe khi nó đến gần chướng ngại vật và tăng tốc khi nó vượt qua chướng ngại
+              vật.</p>
+            <p class="ml-4">+ giảm tốc độ xe khi đi vào khúc cua, đoạn cần rẽ.</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
-}
+};
+
+export default Home;
